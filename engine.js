769efe4041b7a1aae7f66258e4d73632c789c514
dailyblogs <64000625+Nahid1234-source@users.noreply.github.com>
2020-12-02 20:45:59 +0600

    Update engine.js

--- engine.js
+++ engine.js
@@ -25,3 +25,20 @@ function(){
     document.querySelector(".navbar").style.display="none";
 });
 
+document.getElementById("button4").addEventListener("click",
+function(){
+    document.querySelector(".bg-modal").style.display = "flex";
+});
+document.querySelector(".close").addEventListener("click", 
+function(){
+    document.querySelector(".bg-modal").style.display="none";
+});
+
+document.getElementById("button5").addEventListener("click",
+function(){
+    document.querySelector(".bg-modal2").style.display = "flex";
+});
+document.querySelector(".close2").addEventListener("click", 
+function(){
+    document.querySelector(".bg-modal2").style.display="none";
+});
